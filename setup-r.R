232be933ca00726359b5f55d675e77cf1c9a8ebb
michbur <michalburdukiewicz@gmail.com>
2018-11-22 23:25:10 +0100

    first commit

---
+++ setup-r.R
@@ -0,0 +1,3 @@
+install.packages(c("ggplot2", "dplyr", "mlr", "auditor", "ranger", "shiny", "plotly", "rmarkdown",
+                   "Rfast", "reshape2"), 
+                 repos = "https://cloud.r-project.org/")
