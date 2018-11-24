9169878d56190f51c0f0f7df87ecaf48fc4929bb
michbur <michalburdukiewicz@gmail.com>
2018-11-24 01:18:56 +0100

    almost!

--- cz1/cz1-1-budowa-modelu.R
+++ cz1/cz1-1-budowa-modelu.R
@@ -34,10 +34,30 @@ generateHyperParsEffectData(optimal_rf)[["data"]] %>%
   theme_bw()
 
 
-opt_dat <- data.frame(optimal_rf[["opt.path"]]) %>% 
+best_par <- data.frame(optimal_rf[["opt.path"]]) %>% 
   filter(mse.test.mean == min(mse.test.mean))
 
-model_rf <- train(makeLearner("regr.ranger", 
-                              num.trees = opt_dat[["num.trees"]],
-                              min.node.size = opt_dat[["min.node.size"]]), 
-                  predict_price)
+worst_par <- data.frame(optimal_rf[["opt.path"]]) %>% 
+  filter(mse.test.mean == max(mse.test.mean))
+
+model_best <- ranger(formula = cena_m2 ~ ., data = mieszkania, num.trees = best_par[["num.trees"]],
+                     min.node.size = best_par[["min.node.size"]])
+
+model_worst <- ranger(formula = cena_m2 ~ ., data = mieszkania, num.trees = worst_par[["num.trees"]],
+                      min.node.size = worst_par[["min.node.size"]])
+
+library(auditor)
+
+audit_best <- audit(model_best, data = mieszkania, y = mieszkania[["cena_m2"]], label = "best", 
+                    predict.function = function(m, data) predict(m, data)[["predictions"]])
+
+audit_worst <- audit(model_worst, data = mieszkania, y = mieszkania[["cena_m2"]], label = "worst", 
+                     predict.function = function(m, data) predict(m, data)[["predictions"]])
+
+plotModelCorrelation(audit_best, audit_worst)
+plotResidualDensity(audit_best, audit_worst)
+plotScaleLocation(audit_best, audit_worst)
+plotTwoSidedECDF(audit_best, audit_worst)
+
+
+save(model_best, model_worst, file = "./cz2/models.RData")
